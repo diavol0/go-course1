d242d061bd356638a1a002630afb915601fb3529
diavol0 <morto_diavolo@bk.ru>
2022-07-02 12:20:59 +0300

    basic constructions language

---
+++ basic_constructions/B_c.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1 , "\n")
+	fmt.Println("7.0/3.0 =", 7.0/3.0 , "\n")
+	fmt.Println(true && false , "\n")
+	fmt.Println(true || false , "\n")
+	fmt.Println(!true , "\n")
+}
